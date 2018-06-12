7be20aaa91354322623325ca2f9c68d49a4d8136
iGhibli <iGhibli@163.com>
2018-06-12 16:01:47 +0800

    完成网络请求列表数据并实现简单展示

--- Demo01/App.js
+++ Demo01/App.js
@@ -1,46 +1,101 @@
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
- * @flow
  */
 
-import React, { Component } from 'react';
+import React, {
+  Component,
+} from 'react';
+
 import {
   Platform,
+  Image,
+  FlatList,
   StyleSheet,
   Text,
-  Image,
-  View
+  View,
 } from 'react-native';
 
-var MOCKED_MOVIES_DATA = [
-	{title: '标题', year: '2015', posters: {thumbnail: 'https://i.loli.net/2017/11/09/5a046546a2a1f.jpg'}},
-];
+var REQUEST_URL = 'https://raw.githubusercontent.com/facebook/react-native/0.51-stable/docs/MoviesExample.json';
 
-type Props = {};
-export default class App extends Component<Props> {
+export default class SampleAppMovies extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+      loaded: false,
+    };
+    // 在ES6中，如果在自定义的函数里使用了this关键字，则需要对其进行“绑定”操作，否则this的指向会变为空
+    // 像下面这行代码一样，在constructor中使用bind是其中一种做法（还有一些其他做法，如使用箭头函数等）
+    this.fetchData = this.fetchData.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  fetchData() {
+    fetch(REQUEST_URL)
+      .then((response) => response.json())
+      .then((responseData) => {
+        // 注意，这里使用了this关键字，为了保证this在调用时仍然指向当前组件，我们需要对其进行“绑定”操作
+        this.setState({
+          data: this.state.data.concat(responseData.movies),
+          loaded: true,
+        });
+
+        // for (var k in responseData.movies){  
+        //   console.log('AAA: ' + k + ' --- ' + responseData.movies[k].posters.thumbnail);
+        // }
+
+        // for (var j in this.state.data){  
+        //   console.log('BBB: ' + j + ' --- ' + this.state.data[j].posters.thumbnail);
+        // }
+
+      });
+  }
+
   render() {
-    var movie = MOCKED_MOVIES_DATA[0];
+    if (!this.state.loaded) {
+      return this.renderLoadingView();
+    }
+
+    return (
+      <FlatList
+        data={this.state.data}
+        renderItem={this.renderMovie}
+        style={styles.list}
+      />
+    );
+  }
+
+  renderLoadingView() {
     return (
       <View style={styles.container}>
-      	<Image
-        	source={{uri: movie.posters.thumbnail}} 
-        	style={styles.thumbnail}
+        <Text>
+          Loading movies...
+        </Text>
+      </View>
+    );
+  }
+
+  renderMovie = ({item}) =>{
+    return (
+      <View style={styles.container}>
+        <Image
+          source={{uri: item.posters.thumbnail}}
+          style={styles.thumbnail}
         />
         <View style={styles.rightContainer}>
-        	<Text style={styles.title}>
-        		{movie.title}
-        	</Text>
-        	<Text style={styles.year}>
-        		{movie.year}
-        	</Text>
+          <Text style={styles.title}>{item.title}</Text>
+          <Text style={styles.year}>{item.year}</Text>
         </View>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
+var styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: 'row',
@@ -49,18 +104,22 @@ const styles = StyleSheet.create({
     backgroundColor: '#F5FCFF',
   },
   rightContainer: {
-  	flex: 1,
+    flex: 1,
+  },
+  title: {
+    fontSize: 20,
+    marginBottom: 8,
+    textAlign: 'center',
+  },
+  year: {
+    textAlign: 'center',
   },
   thumbnail: {
     width: 53,
     height: 81,
   },
-  title: {
-  	fontSize: 20,
-  	marginBottom: 8,
-  	textAlign: 'center',
-  },
-  year: {
-  	textAlign: 'center',
+  list: {
+    paddingTop: 20,
+    backgroundColor: '#F5FCFF',
   },
 });
