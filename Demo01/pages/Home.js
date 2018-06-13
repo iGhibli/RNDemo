8d25f2198f4fae4b43116669d6f03e858f342887
iGhibli <iGhibli@163.com>
2018-06-13 23:20:33 +0800

    添加column显示

--- Demo01/pages/Home.js
+++ Demo01/pages/Home.js
@@ -9,13 +9,22 @@ import Swiper from 'react-native-swiper';
 import {
     View,
     Image,
-    Text, StyleSheet
+    Text,
+    TouchableHighlight,
+    StyleSheet
 } from 'react-native'
 
 var sliderImgs = [
     'http://images3.c-ctrip.com/SBU/apph5/201505/16/app_home_ad16_640_128.png',
     'http://images3.c-ctrip.com/rk/apph5/C1/201505/app_home_ad49_640_128.png',
     'http://images3.c-ctrip.com/rk/apph5/D1/201506/app_home_ad05_640_128.jpg'
+];
+
+var BUIcon = [
+    'https://raw.githubusercontent.com/vczero/vczero.github.io/master/ctrip/%E6%9C%AA%E6%A0%87%E9%A2%98-1.png',
+    'https://raw.githubusercontent.com/vczero/vczero.github.io/master/ctrip/feiji.png',
+    'https://raw.githubusercontent.com/vczero/vczero.github.io/master/ctrip/lvyou.png',
+    'https://raw.githubusercontent.com/vczero/vczero.github.io/master/ctrip/gonglue.png'
 ];
 
 class Slider extends React.Component {
@@ -33,8 +42,65 @@ class Slider extends React.Component {
 export default class Home extends Component {
     render() {
         return(
-            <View style={{flex:1, backgroundColor: 'red', marginTop: 64}}>
-                <Slider/>
+            <View style={{flex:1, backgroundColor: '#F2F2F2', marginTop: 64}}>
+                <View style={{height: 80}}>
+                    <Slider/>
+                </View>
+                <View style={{flexDirection: 'column', height: 400}}>
+                    // part.1
+                    <View style={{flex: 1, backgroundColor: 'red'}}>
+                        <TouchableHighlight underlayColor={'#FA6778'} style={{flex: 1}}>
+                            <View style={[styles.sbu_flex, styles.sbu_borderRight]}>
+                                <View>
+                                    <Text>酒店</Text>
+                                </View>
+                                <View>
+                                    <Image style={{height: 40, width: 40, resizeMode:Image.resizeMode.contain}} source={{uri:BUIcon[0]}}></Image>
+                                </View>
+                            </View>
+                        </TouchableHighlight>
+                    </View>
+                    // part.2
+                    <View style={{flex: 1, backgroundColor: 'orange'}}>
+                        <TouchableHighlight underlayColor={'#FA6778'} style={{flex: 1}}>
+                            <View style={[styles.sbu_flex, styles.sbu_borderRight]}>
+                                <View>
+                                    <Text>酒店</Text>
+                                </View>
+                                <View>
+                                    <Image style={{height: 40, width: 40, resizeMode:Image.resizeMode.contain}} source={{uri:BUIcon[0]}}></Image>
+                                </View>
+                            </View>
+                        </TouchableHighlight>
+                    </View>
+                    // part.3
+                    <View style={{flex: 1, backgroundColor: 'yellow'}}>
+                        <TouchableHighlight underlayColor={'#FA6778'} style={{flex: 1}}>
+                            <View style={[styles.sbu_flex, styles.sbu_borderRight]}>
+                                <View>
+                                    <Text>酒店</Text>
+                                </View>
+                                <View>
+                                    <Image style={{height: 40, width: 40, resizeMode:Image.resizeMode.contain}} source={{uri:BUIcon[0]}}></Image>
+                                </View>
+                            </View>
+                        </TouchableHighlight>
+                    </View>
+                    // part.4
+                    <View style={{flex: 1, backgroundColor: 'green'}}>
+                        <TouchableHighlight underlayColor={'#FA6778'} style={{flex: 1}}>
+                            <View style={[styles.sbu_flex, styles.sbu_borderRight]}>
+                                <View>
+                                    <Text>酒店</Text>
+                                </View>
+                                <View>
+                                    <Image style={{height: 40, width: 40, resizeMode:Image.resizeMode.contain}} source={{uri:BUIcon[0]}}></Image>
+                                </View>
+                            </View>
+                        </TouchableHighlight>
+                    </View>
+                </View>
+
                 <Text>Hello, Jason!</Text>
             </View>
         );
